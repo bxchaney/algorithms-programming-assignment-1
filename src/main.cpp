305fbf64f7d16d47564fab8bf6fa753428b50c1b
bxchaney <bxchaney@gmail.com>
2023-10-01 17:49:39 -0400

    added to main/tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,52 @@
 /*
  * Copyright 2023 bxchaney
  */
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <streambuf>
+#include <string>
 
-int main() {
-    int x = 5;
-    std::cout << "Hello, World!" << std::endl;
+#include "utils/close.h"
+#include "utils/container.hpp"
+#include "utils/point.hpp"
+#include "utils/point_reader.h"
+
+/// @brief this function casts a null terminated char array to its integer
+/// value. It assumes that the array is only integer numerals.
+int cast_char_arr_to_int(char* str) {
+    int n{0};
+    int i{0};
+    while (str[i]) {
+        n *= 10;
+        n += (str[i] - '0');
+        i++;
+    }
+    return n;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        return -1;
+    }
+
+    // assuming that the filepath to the datafile is the first parameter
+    std::ifstream ifs{argv[1]};
+    if (!ifs.is_open()) {
+        std::cout << "Could not open file."
+                  << " Please ensure that the first parameter is a valid "
+                  << "relative filepath." << std::endl;
+        return -1;
+    }
+
+    Queue<Point<uint16_t>> q{};
+
+    read_points(ifs, &q);
+
+    Point<uint16_t>* p{q.to_array()};
+
+    get_closest_points(p, (int)q.size());
+
+    delete[] p;
 }
--- src/tests.cpp
+++ src/tests.cpp
@@ -2,11 +2,28 @@
  * Copyright 2023, Brandon Chaney
  */
 
+#include <time.h>
+
+#include <cstdint>
+#include <cstdlib>
 #include <iostream>
 
-#include "utils/container.h"
-#include "utils/point.h"
-#include "utils/tree.h"
+#include "utils/mergesort.hpp"
+#include "utils/container.hpp"
+#include "utils/point.hpp"
+#include "utils/point_reader.h"
+#include "utils/tree.hpp"
+
+void test_hash() {
+    uint16_t x{65535};
+    uint16_t y{65535};
+    uint32_t hash{get_hash(x, y)};
+    std::cout << hash << std::endl;
+}
+
+bool cmp(int* a, int* b) { return *a <= *b; }
+
+bool call_cmp(int a, int b, bool (*cmp)(int*, int*)) { return cmp(&a, &b); }
 
 int main() {
     using int_point = Point<int>;
@@ -20,14 +37,23 @@ int main() {
 
     std::cout << "Emptying stack" << std::endl;
     while (!s.is_empty()) {
-        int_point p = s.pop();
+        int_point p{s.pop()};
         q.enqueue(p);
+        std::cout << "queued p" << std::endl;
         std::cout << p << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Printing queue" << std::endl;
     std::cout << q << std::endl;
+
+    Point<int>* arr{q.to_array()};
+    int q_size = q.size();
+
+    std::cout << "printing arr contents" << std::endl;
+    for (int i = 0; i < q_size; i++) {
+        std::cout << arr[i] << std::endl;
+    }
 
     Tree<int> t{};
 
@@ -38,4 +64,27 @@ int main() {
     std::cout << t.search(5) << std::endl;
     std::cout << t.search(999) << std::endl;
     std::cout << t.search(1000) << std::endl;
+    test_hash();
+
+    srand(time(0));
+
+    int* int_arr{new int[20]};
+    for (int i = 0; i < 20; i++) {
+        int_arr[i] = (rand() % 50);
+    }
+    std::cout << "random array: ";
+    for (int i = 0; i < 20; i++) {
+        std::cout << int_arr[i] << " ";
+    }
+    std::cout << std::endl;
+    merge_sort<int>(int_arr, 0, 19, &cmp);
+    std::cout << "sorted array: ";
+    for (int i = 0; i < 20; i++) {
+        std::cout << int_arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int a{4};
+    int b{5};
+    std::cout << call_cmp(a, b, &cmp) << std::endl;
 }
