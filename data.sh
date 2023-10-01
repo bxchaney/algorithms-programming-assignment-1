325b1fd794c51fc88f57d013e98649c045e1173c
bxchaney <bxchaney@gmail.com>
2023-10-01 12:02:05 -0400

    changed data range and dataset sizes

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 mkdir -vp data
-python ./make_points.py 50 1000 > ./data/dataset1.txt 
-python ./make_points.py 50 1000 > ./data/dataset2.txt 
-python ./make_points.py 50 1000 > ./data/dataset3.txt 
-python ./make_points.py 50 1000 > ./data/dataset4.txt 
-python ./make_points.py 50 1000 > ./data/dataset5.txt 
+python ./make_points.py 100 65535 > ./data/dataset1.txt
+python ./make_points.py 1000 65535 > ./data/dataset2.txt
+python ./make_points.py 10000 65535 > ./data/dataset3.txt
+python ./make_points.py 100000 65535 > ./data/dataset4.txt
+python ./make_points.py 1000000 65535 > ./data/dataset5.txt
