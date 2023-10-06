e64f12f31ba30c6323c892ef629737a832710cca
bxchaney <bxchaney@gmail.com>
2023-10-06 13:13:41 -0400

    added arrayqueue

--- src/utils/container.hpp
+++ src/utils/container.hpp
@@ -233,6 +233,80 @@ class Queue {
 };
 
 template <typename T>
+class ArrayQueue {
+    using value_type = T;
+    using value_pointer = T *;
+
+    size_t _size;
+    value_pointer _arr;
+    size_t _max_size;
+    int _front;
+
+ public:
+    ArrayQueue() {
+        // default allocated space is 20
+        _arr = new value_type[20];
+        _size = 0;
+        _max_size = 20;
+    }
+
+    explicit ArrayQueue(int n) {
+        // may specify a different starting allocation
+        _arr = new value_type[n];
+        _size = 0;
+        _max_size = n;
+    }
+
+    ~ArrayQueue() { delete[] _arr; }
+
+    void resize(int n) {
+        value_pointer new_arr{new value_type[n]};
+        for (int i = 0; i < static_cast<int>(_size); i++) {
+            new_arr[i] = _arr[(_front + i) % _max_size];
+        }
+
+        _max_size = n;
+        _front = 0;
+        value_pointer old_arr{_arr};
+        _arr = new_arr;
+        delete[] old_arr;
+    }
+
+    void enqueue(value_type data) {
+        if (_size == _max_size) {
+            resize(_max_size * 2);
+        }
+        if (_size == 0) {
+            _front = 0;
+            _arr[0] = data;
+        } else {
+            // queue wraps around
+            _arr[(_front + _size) % _max_size] = data;
+        }
+        _size++;
+    }
+
+    value_type dequeue() {
+        if (_size == 0) throw std::exception();
+        value_type data{_arr[_front]};
+        _front = (_front + 1) % _max_size;
+        _size--;
+        if (_size < (_max_size / 4) && (_max_size > 40)) { resize(_max_size / 2);}
+        return data;
+    }
+
+    size_t size() { return _size; }
+
+    value_pointer to_array() {
+        value_pointer p{new value_type[_size]};
+        for (int i = 0; i < static_cast<int>(_size); i++) {
+            p[i] = _arr[(_front + i) % _max_size];
+        }
+        return p;
+    }
+};
+
+template <typename T>
 class ArrayList {
     using value_type = T;
     using pointer_type = T *;
