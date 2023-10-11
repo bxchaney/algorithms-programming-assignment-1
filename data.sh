0a66547444b1e23bd19546f35ddda35c56f6391a
bxchaney <bxchaney@gmail.com>
2023-10-11 09:48:15 -0400

    updated data ranges

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 mkdir -vp data
-python ./make_points.py 100 65535 > ./data/dataset1.txt
-python ./make_points.py 1000 65535 > ./data/dataset2.txt
-python ./make_points.py 10000 65535 > ./data/dataset3.txt
-python ./make_points.py 100000 65535 > ./data/dataset4.txt
-python ./make_points.py 1000000 65535 > ./data/dataset5.txt
+python ./make_points.py 100 10000 > ./data/dataset1.txt
+python ./make_points.py 1000 10000 > ./data/dataset2.txt
+python ./make_points.py 10000 10000 > ./data/dataset3.txt
+python ./make_points.py 100000 10000 > ./data/dataset4.txt
+python ./make_points.py 1000000 10000 > ./data/dataset5.txt
