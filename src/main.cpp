6799c502cff5c1a2e45a97a03c4d4a97c9cd7d2c
bxchaney <bxchaney@gmail.com>
2023-10-11 13:31:53 -0400

    finished main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 /*
  * Copyright 2023 bxchaney
  */
-#include <cstdint>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <streambuf>
-#include <string>
 
+#include <cstdint>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+
+#include "utils/brute_force.h"
 #include "utils/close.h"
 #include "utils/container.hpp"
+#include "utils/efficient.h"
 #include "utils/point.hpp"
 #include "utils/point_reader.h"
+
+using PointPair = PointComparer::PointPair;
 
 /// @brief this function casts a null terminated char array to its integer
 /// value. It assumes that the array is only integer numerals.
@@ -24,6 +28,56 @@ int cast_char_arr_to_int(char* str) {
         i++;
     }
     return n;
+}
+
+void trace_run(Point<double>* p, int arr_size, int m) {
+    BruteForceClosestPairs brute_force{};
+    EfficientClosestPairs efficient{};
+
+    std::cout << "Finding the " << m
+              << " closest pairs amongst the following points:" << std::endl;
+    std::cout << "[ ";
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << p[i] << " ";
+    }
+    std::cout << "]" << std::endl << std::endl;
+
+    std::unique_ptr<ArrayList<PointPair>> brute_force_pp{
+        brute_force.brute_force_closest_pairs(p, arr_size, m)};
+    std::unique_ptr<ArrayList<PointPair>> efficient_pp{
+        efficient.divide_and_conquer_closest_pairs(p, arr_size, m)};
+
+    bool results_match = true;
+    if (brute_force_pp->size() != efficient_pp->size()) {
+        results_match = false;
+    }
+
+    std::cout << "Brute force closest pairs: " << std::endl;
+    std::unique_ptr<PointPair[]> bf_arr{brute_force_pp->to_array()};
+    int brute_force_size{static_cast<int>(brute_force_pp->size())};
+    for (int i = 0; i < brute_force_size; i++) {
+        std::cout << std::setw(3) << (i + 1) << ". "
+                  << "Pair( distance: " << std::setw(8) << bf_arr[i].distance
+                  << ", p1: " << bf_arr[i].first << ", p2: " << bf_arr[i].second
+                  << " )" << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Efficient method closest pairs: " << std::endl;
+    std::unique_ptr<PointPair[]> eff_arr{efficient_pp->to_array()};
+    int eff_size{static_cast<int>(efficient_pp->size())};
+    for (int i = 0; i < eff_size; i++) {
+        if (eff_arr[i] != bf_arr[i]) results_match = false;
+        std::cout << std::setw(3) << (i + 1) << ". "
+                  << "Pair( distance: " << std::setw(8) << eff_arr[i].distance
+                  << ", p1: " << eff_arr[i].first
+                  << ", p2: " << eff_arr[i].second << " )" << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << std::endl
+              << "Results match: " << (results_match ? "True" : "False")
+              << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -40,26 +94,47 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    ArrayQueue<Point<uint16_t>> q{};
+    int n{cast_char_arr_to_int(argv[2])};
+
+    ArrayQueue<Point<double>> q{};
 
     read_points(ifs, &q);
 
-    Point<uint16_t>* p{q.to_array()};
+    Point<double>* p{q.to_array()};
+    uint64_t dataset_size{q.size()};
+    uint64_t max_m{((dataset_size - 1) * dataset_size) / 2};
 
-    //    get_closest_points(p, (int)q.size());
+    if (static_cast<uint64_t>(n) > max_m) {
+        std::cout << "The provided m value was too large for this dataset."
+                  << std::endl;
+        std::cout << "This dataset has " << dataset_size
+                  << " points. The max allowed m value is: " << max_m
+                  << std::endl;
+        std::cout << "You entered: " << n << std::endl;
+        delete[] p;
+        return -1;
+    }
 
-    ArrayList<PointPair>* pp{
-        divide_and_conquer_closest_pairs(p, static_cast<int>(q.size()), 100)};
+    // trace run to compare brute force and efficient
+    if (argc == 3) {
+        trace_run(p, static_cast<int>(q.size()), n);
+    } else {
+        int run_type{cast_char_arr_to_int(argv[3])};
 
-    PointPair* pp_arr{pp->to_array()};
-    for (int i = 0; i < static_cast<int>(pp->size()); i++) {
-        Point<uint16_t> p1{pp_arr[i].first};
-        Point<uint16_t> p2{pp_arr[i].second};
-        double dist{pp_arr[i].distance};
-        std::cout << p1 << " " << p2 << " " << dist << std::endl;
+        if (run_type == 0) {
+            BruteForceClosestPairs comp{};
+            std::unique_ptr<ArrayList<PointPair>> pairs{
+                comp.brute_force_closest_pairs(p, dataset_size, n)};
+            std::cout << argv[1] << ",brute-force," << dataset_size << "," << n
+                      << "," << comp.comparisons() << std::endl;
+        } else {
+            EfficientClosestPairs comp{};
+            std::unique_ptr<ArrayList<PointPair>> pairs{
+                comp.divide_and_conquer_closest_pairs(p, dataset_size, n)};
+            std::cout << argv[1] << ",efficient," << dataset_size << "," << n
+                      << "," << comp.comparisons() << std::endl;
+        }
     }
 
     delete[] p;
-    delete[] pp_arr;
-    delete pp;
 }
